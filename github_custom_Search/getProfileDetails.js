305cff0c2efdb079ee9158227e2ceb04b0ff2bbb
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-07-05 13:14:12 -0400

    Update getProfileDetails.js

--- github_custom_Search/getProfileDetails.js
+++ github_custom_Search/getProfileDetails.js
@@ -32,11 +32,12 @@ function downloadr(arr2D, filename) {
   }
 }
 
-async function getPatches(link,fullname) {
+async function getPatches(link,fullname,path) {
   var res = await fetch(link);
   var html = await res.text();
   var email = reg(/[\w\.+]+@\S+\.[a-zA-Z]+/.exec(html.replace(/\w+@users.noreply.github.com|\+.+?(?=@)/g, '')),0);
-  var check = reg(new RegExp('from:\\W*'+fullname, 'i').test(html)) && /\w+@users.noreply.github.com/i.test(email) === false ?  email : '';
+  var rstr = 'from:\\W*' + fullname.replace(/\W+/g, '\\W+') + '|from:\\W*' + path.replace(/\W+/g, '\\W+');
+  var check = reg(new RegExp(rstr, 'i').test(html)) && /\w+@users.noreply.github.com/i.test(email) === false ?  email : '';
   return check;
 }
 
@@ -84,7 +85,7 @@ async function loopThroughRepos(path,primaryLang){
   if(email == null || email.length == 0){
     for(var r=0; r<owns.length; r++){
       var link = `https://github.com/${path}/${owns[r].repo}/commit/master.patch`;
-      var patchEmail = await getPatches(link,fullname);
+      var patchEmail = await getPatches(link,fullname,path);
       if(patchEmail) {console.log(patchEmail); email.push(patchEmail); break};
     }
   }
