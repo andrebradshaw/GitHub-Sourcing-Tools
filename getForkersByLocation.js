efefd2c5f50b808fff19b7f6554242e90e157b64
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-07-10 08:11:54 -0400

    Update getForkersByLocation.js

--- getForkersByLocation.js
+++ getForkersByLocation.js
@@ -9,11 +9,60 @@ var delay = (ms) => new Promise(res => setTimeout(res, ms));
 var ele = (t) => document.createElement(t);
 var attr = (o, k, v) => o.setAttribute(k, v);
 
+function convertToTSV(fileData) {
+  var firstLevel = fileData.map(el => Object.entries(el));
+  var lens = Math.max(...firstLevel.map(el => el.length));
+  var header = unq(firstLevel.map(el => el.map(itm => itm[0])).flat());
+  var table = [header];
+  var str = (o) => typeof o == 'object' ? JSON.stringify(o).replace(/\n|\r/g, ' ') : o.toString().replace(/\n|\r/g, ' ');
+  for (var i = 0; i < firstLevel.length; i++) {
+    var arr = [];
+    var row = [];
+    for (var s = 0; s < firstLevel[i].length; s++) {
+      var place = header.indexOf(firstLevel[i][s][0]);
+      arr[place] = firstLevel[i][s][1];
+    }
+    for (var a = 0; a < arr.length; a++) {
+      if (arr[a]) {
+        row.push(arr[a]);
+      } else {
+        row.push('');
+      }
+    }
+    table.push(row);
+  }
+}
+
+function parseAsRegexArr(bool) {
+  function rxReady(s){ return s ? s.replace(/"/g, '\\b').trim().replace(/\)/g, '').replace(/\(/g, '').replace(/\s+/g, '.{0,2}').replace(/\//g, '\\/').replace(/\+/g, '\\+').replace(/\s*\*\s*/g, '\\s*\\w*\\s+') : s;}
+  function checkSimpleOR(s) { return /\bor\b/i.test(s) && /\(/.test(s) === false;}
+  if (checkSimpleOR(bool)) {
+    var x = new RegExp(bool.replace(/\s+OR\s+|\s*\|\s*/gi, '|').replace(/\//g, '\\/').replace(/"/g, '\\b').replace(/\s+/g, '.{0,2}').replace(/\s*\*\s*/g, '\\s*\\w*\\s+'), 'i');
+    var xArr = [x];
+    return xArr;
+  } else {
+    var orx = "\\(.+?\\)|(\\(\\w+\\s{0,1}OR\\s|\\w+\\s{0,1}OR\\s)+((\\w+\s)+?|(\\w+)\\)+)+?";
+    var orMatch = bool ? bool.match(new RegExp(orx, 'g')) : [];
+    var orArr = orMatch ? orMatch.map(function(b) {return rxReady(b.replace(/\s+OR\s+|\s*\|\s*/gi, '|'))}) : [];
+    var noOrs = bool ? bool.replace(new RegExp(orx, 'g'), '').split(/\s+[AND\s+]+/i) : bool;
+    var ands = noOrs ? noOrs.map(function(a) { return rxReady(a)}) : [];
+    var xArr = ands.concat(orArr).filter(function(i){ return i != ''}).map(function(x){return new RegExp(x, 'i')});
+    return xArr;
+  }
+}
+
+function booleanSearch(bool,target){
+  var arr = parseAsRegexArr(bool);
+  return arr.every(function(x){
+    return x.test(target);
+  });
+}
+
 async function getHoverCard(userId,geoSearch){
   var res = await fetch("https://github.com/hovercards?user_id="+userId, {"credentials":"include","headers":{"accept":"*/*","accept-language":"en-US,en;q=0.9","x-requested-with":"XMLHttpRequest"}});
   var text = await res.text();
   var doc = new DOMParser().parseFromString(text, 'text/html');
-  return geoSearch.test(doc.body.innerText);
+  return booleanSearch(geoSearch,doc.body.innerText);
 }
 
 async function getForkerIdsByRepo(path){
@@ -26,9 +75,9 @@ async function getForkerIdsByRepo(path){
   return repos;
 }
 
-async function loopThroughForkersSearchGeo(geoSearch){
+async function loopThroughForkersSearchGeo(geoSearch,repoPath){
   var matchingProfilePaths = [];
-  var forkers = await getForkerIdsByRepo('google/shaka-player');
+  var forkers = await getForkerIdsByRepo(repoPath);
   for(var i=0; i<forkers.length; i++){
     var geoMatch = await getHoverCard(forkers[i][1]);
     if(geoMatch) matchingProfilePaths.push(forkers[i][0]);
@@ -37,3 +86,132 @@ async function loopThroughForkersSearchGeo(geoSearch){
   return unq(matchingProfilePaths); 
 }
 
+
+var cleanObject = (ob) => 
+  Object.entries(ob).reduce((r, [k, v]) => {
+    if(v) { r[k] = v; return r;
+    } else { return r; }
+  }, {});
+
+function downloadr(arr2D, filename) {
+  var data = /\.json$|.js$/.test(filename) ? JSON.stringify(arr2D) : arr2D.map(el=> el.reduce((a,b) => a+'\t'+b )).reduce((a,b) => a+'\r'+b);
+  var type = /\.json$|.js$/.test(filename) ? 'data:application/json;charset=utf-8,' : 'data:text/plain;charset=utf-8,';
+  var file = new Blob([data], {    type: type  });
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  } else {
+    var a = document.createElement('a'),
+    url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 10);
+  }
+}
+
+async function getPatches(link) {
+  var res = await fetch(link);
+  var html = await res.text();
+  var email = reg(/[\w\.+]+@\S+\.[a-zA-Z]+/.exec(html.replace(/\w+@users.noreply.github.com|\+.+?(?=@)/g, '')),0);
+  var check = /users.noreply.github.com/i.test(email) ? '' : email;
+  return check;
+}
+
+async function getProfileRepoData(url) {
+  var res = await fetch(url);
+  var text = await res.text();
+  var doc = new DOMParser().parseFromString(text, 'text/html');
+  return doc;
+}
+
+function parseRepo(doc,type){
+  return Array.from(cn(doc,`col-12 d-flex width-full py-4 border-bottom public ${type}`)).map(el=> {
+   var stars = cn(el, 'octicon-star')[0] ? cn(el, 'octicon-star')[0].parentElement.innerText.trim() : 0;
+   return {
+     repo: reg(/(?<=github.com\/.+?\/).+?$/.exec(tn(el,'a')[0].href),0),
+     lang: cn(el, 'ml-0 mr-3')[0] ? cn(el, 'ml-0 mr-3')[0].innerText.trim() : '',
+     time: tn(el,'relative-time')[0] ? new Date(tn(el,'relative-time')[0].getAttribute('datetime')).getTime() : 0,
+     stars: stars == 'Unstar' ? 0 : parseInt(stars.replace(/\D+/g,'')),
+     forks: cn(el,'octicon octicon-repo-forked')[0] ? parseInt(cn(el,'octicon octicon-repo-forked')[0].parentElement.innerText.trim().replace(/\D+/g,'')) : 0
+   };
+  });
+}
+
+function getFollowCounts(elm,type){
+  var follower_a = Array.from(tn(elm,'a')).filter(el=> type.test(el.innerText))[0];
+  var follower_s = follower_a ? cn(follower_a,'Counter') : null;
+  var followerCount = follower_s[0] ? parseInt(follower_s[0].innerText.trim()) : 0;
+  return followerCount;
+}
+
+async function loopThroughRepos(path){
+  var prop = (arr,str) => arr.filter(el=> el.getAttribute('itemprop') == str).map(el=> el ? el.innerText.trim() : '');
+  var res = await getProfileRepoData(`https://github.com/${path}?tab=repositories`);
+  var mainDoc = await getProfileRepoData(`https://github.com/${path}`);
+  var owns = parseRepo(res,'source');
+  var forks = parseRepo(res,'fork');
+  var fullname = cn(res,'vcard-fullname')[0] ? cn(res,'vcard-fullname')[0].innerText : '';
+  var vcard = cn(res,'vcard-details ')[0] ? Array.from(tn(cn(res,'vcard-details ')[0],'li')) : null;
+  var geo = vcard ? prop(vcard,'homeLocation') : null;
+  var email = vcard ? prop(vcard,'email') : null;
+  var website = vcard ? prop(vcard,'url') : null;
+  var worksFor = vcard ? prop(vcard,'worksFor') : null;
+  var followers = getFollowCounts(res,/followers/i);
+  var following = getFollowCounts(res,/following/i);
+  var bio = cn(res,'p-note user-profile-bio js-user-profile-bio')[0] ? cn(res,'p-note user-profile-bio js-user-profile-bio')[0].innerText.trim() : '';
+  var contributions = cn(mainDoc,'graph-before-activity-overview')[0] ? Array.from(cn(cn(mainDoc,'graph-before-activity-overview')[0],'day')).map(el=> {return {date: new Date(el.getAttribute('data-date')).getTime(), commits: parseInt(el.getAttribute('data-count'))}}).filter(el=> el.commits) : null;
+  var pagenate = cn(res,'paginate-container')[0] ? Array.from(tn(cn(res,'paginate-container')[0],'a')).filter(el=> el.innerText == 'Next').map(el=> el.href) : [];
+  var pages = cn(res,'UnderlineNav-item mr-0 mr-md-1 mr-lg-3 selected ')[0] ? Math.ceil(parseInt(cn(res,'UnderlineNav-item mr-0 mr-md-1 mr-lg-3 selected ')[0].innerText.replace(/\D+/g,''))/30) : 0;
+  for(var i=2; i<=pages; i++){
+    var res2 = await getProfileRepoData(pagenate[0]);
+    parseRepo(res2,'fork').forEach(el=> forks.push(el));
+    parseRepo(res2,'source').forEach(el=> owns.push(el));
+  }
+  if(email == null || email.length == 0){
+    for(var r=0; r<owns.length; r++){
+      var link = `https://github.com/${path}/${owns[r].repo}/commit/master.patch`;
+      var patchEmail = await getPatches(link);
+      if(patchEmail) {console.log(patchEmail); email.push(patchEmail); break};
+    }
+  }
+  var langs = unq(owns.map(el=> el.lang).filter(el=> el != ''));
+  var interest = unq(forks.map(el=> el.lang).filter(el=> el != ''));
+  var recognized = owns.filter(el=> (el.forks > 0 || el.stars > 0) && el.lang).sort((a,b) => b.time - a.time);
+  var profile = {
+    fullname: fullname ? fullname : null,
+    bio: bio ? bio : null,
+    github: 'github.com/'+path,
+    followers: followers,
+    following: following,
+    geo: geo ? geo.toString(): null,
+    worksFor: worksFor && worksFor.length > 0 ? worksFor.toString() : null,
+    email: email && email.length > 0 ? unq(email) : null,
+    website: website && website.length > 0 ? website.toString() : null,
+    langs: langs && langs.length > 0 ? langs : null,
+    interest: interest && interest.length > 0 ? interest : null,
+    owns: owns.length > 0 ? owns : null,
+    forks: forks.length > 0 ? forks : null,
+    recognized: recognized && recognized.length > 0 ? recognized : null,
+    contributions: contributions && contributions.length > 0 ? contributions : null,
+    totalContributions: contributions && contributions.length > 0 ? contributions.map(el=> el.commits).reduce((a,b) => a+b) : null
+  }
+  return cleanObject(profile);
+}
+
+async function getMatchingProfiles(geoSearch,targetRepo){
+  var containArr = [];
+  var profiles = loopThroughForkersSearchGeo(geoSearch,targetRepo)
+  for(var i=0; i<profiles.length; i++){
+	var userj = await loopThroughRepos(profiles[i]);
+    containArr.push(userj);
+    await delay(rando(133)+1303);
+  }
+  var output = convertToTSV(containArr);
+  await delay(1111);
+  downloadr(output,geoSearch+'+forks_'+targetRepo+'.tsv')
+}
+getMatchingProfiles('Germany','google/shaka-player');
